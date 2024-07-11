325ed85392ca769e2d4bdf9dd96894074c1c9b3b
Devashish Sarmah <devashish.dev.sarmah@gmail.com>
2024-07-11 22:33:39 +0530

    fix: integrate redis microservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { appConfig, typeOrmConfig } from './app.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostModule } from './modules/post/post.module';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -20,16 +19,6 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
       inject: [ConfigService],
     }),
     PostModule,
-    ClientsModule.register([
-      {
-        name: 'REDIS',
-        transport: Transport.REDIS,
-        options: {
-          host: process.env.REDIS_HOST,
-          port: Number(process.env.REDIS_PORT),
-        },
-      },
-    ]),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -3,9 +3,22 @@ import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './post.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [
+    TypeOrmModule.forFeature([Post]),
+    ClientsModule.register([
+      {
+        name: 'REDIS',
+        transport: Transport.REDIS,
+        options: {
+          host: process.env.REDIS_HOST,
+          port: Number(process.env.REDIS_PORT),
+        },
+      },
+    ]),
+  ],
   controllers: [PostController],
   providers: [PostService],
 })
